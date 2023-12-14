35eaf106098173db96abab792f7ef9e4585a16ef
Patricia <kovach.patri@gmail.com>
2023-12-14 17:58:43 +0100

    data fetching for static pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a9578b088b4bed4f"; }
+/******/ 		__webpack_require__.h = function() { return "08b82cd4ebe58185"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
