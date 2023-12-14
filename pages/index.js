fe52c56fb7b3c369b92c7b4d11616d7f50bbc2fa
Patricia <kovach.patri@gmail.com>
2023-12-14 15:23:08 +0100

    outputting a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/800px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg",
+    address: "Some address 5, 1212 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "Second",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg/800px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu.jpg",
+    address: "Some address 10, 1232 Some City",
+    description: "This is a second meetup",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+export default HomePage;
