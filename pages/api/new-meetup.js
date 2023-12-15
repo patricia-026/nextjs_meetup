59e9a9f43d3a1c88e6eac32283fd2456951d6855
Patricia <kovach.patri@gmail.com>
2023-12-15 10:38:12 +0100

    working with mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,29 @@
+import { MongoClient } from "mongodb";
+
 //api/new-meetup
 //ONLY POST
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    //const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      //ez soha nem futhat client oldalon, de ez a fv nem is fog ott futni
+      "mongodb+srv://admin:pass123@cluster0.xkrycyt.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    //ez a meetups más is lehet mint a fenti
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    //ezeket mind try-catch-be ha valami error van
+
+    console.log(result);
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 }
 
